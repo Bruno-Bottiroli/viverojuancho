f532fee9d9ebb02804619abfe42cf3168c4a32ce
Bruno-Bottiroli <bottirolibruno3@gmail.com>
2024-06-15 14:14:08 -0300

    .

--- js/index.js
+++ js/index.js
@@ -1,13 +1,6 @@
 const botoncarrito = document.querySelector("img#imgcarrito")
 
-function iracheckout(){
-    location.href="./pages/checkout.html"
-}
-function botoncarro(){
-    if (carrito.length > 0){
-        botoncarrito.addEventListener("click", iracheckout())
-    }
-}
+
 botoncarrito.addEventListener("click", ()=> {
-    carrito.length > 0 ? location.href = "./pages/checkout.html" : alert("⛔️ Cargue al menos un producto en el carrito.")
+    location.href = "./pages/checkout.html" 
 })
